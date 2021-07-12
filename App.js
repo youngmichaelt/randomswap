f192a116be47d8314f5b8c43c9df523b1c7aa3e2
Mac <mac@Macs-MacBook.local>
2021-07-11 20:01:33 -0400

    Added mongodb.

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ var app = express();
 var cors = require('cors')
 var stringify = require('qs-stringify')
 var qs = require('qs');
-
-
+var mongo = require('mongodb');
 //const swap = require('./swap.js');
 
 app.use(cors())
@@ -12,44 +11,36 @@ app.use(cors())
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-   //res.send('Hello World');
-   res.sendFile('public/index.html');
-    
-
-        
-            
-
-   //swap;
-   //console.log(swap)
-   
-})
-app.get('/test', function (req, res) {
+app.get('/', function(req, res) {
     //res.send('Hello World');
-    res.sendFile('/Users/mac/Desktop/randomswap/public/test.html');
-     
- 
-         
-             
- 
+    res.sendFile('public/index.html');
+
+
     //swap;
     //console.log(swap)
-    
- })
+
+})
+app.get('/test', function(req, res) {
+    //res.send('Hello World');
+    res.sendFile('/Users/mac/Desktop/randomswap/public/test.html');
+    //swap;
+    //console.log(swap)
+
+})
 
 
 
 
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-   const params = {
-    buyToken: 'DAI',
-    sellToken: 'ETH',
-    sellAmount: '1000000000000000000', // Always denominated in wei
-}
-console.log(`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`);
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    //    console.log("Example app listening at http://%s:%s", host, port)
+    //    const params = {
+    //     buyToken: 'DAI',
+    //     sellToken: 'ETH',
+    //     sellAmount: '1000000000000000000', // Always denominated in wei
+    // }
+    // console.log(`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`);
 })
--- public/swap.mjs
+++ public/swap.mjs
@@ -1,4 +1,3 @@
-
 window.addEventListener("load", async function() {
     const web3 = new Web3(window.ethereum);
     let accounts = await web3.eth.getAccounts();
@@ -11,7 +10,7 @@ window.addEventListener("load", async function() {
 
 
 
-async function mmConnect(){
+async function mmConnect() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     // Prompt user for account connections
     await provider.send("eth_requestAccounts", []);
@@ -30,7 +29,7 @@ async function mmConnect(){
 };
 
 
-async function swap(token1, token2, amount, tokens){
+async function swap(token1, token2, amount, tokens) {
 
     // console.log(getTokens());
 
@@ -41,20 +40,20 @@ async function swap(token1, token2, amount, tokens){
         buyAmount: '1000000000000000000', // Always denominated in wei
     }
     const amountInWei = amount * 1000000000000000000
-    
+
     // const response = await fetch(
     //     //`https://ropsten.api.0x.org/swap/v1/quote?${JSON.stringify(params)}`
     //     `https://ropsten.api.0x.org/swap/v1/quote?sellToken=`+token1+`&buyToken=`+token2+`&buyAmount=3000000000000000000`
     //     //`https://api.0x.org/swap/v1/quote?buyToken=DAI&sellToken=ETH&buyAmount=1000000000000000000`
     // );
-    url = `https://ropsten.api.0x.org/swap/v1/quote?sellToken=`
-    +token1+`&buyToken=`
-    +token2+`&sellAmount=`
-    +amountInWei + `&feeRecipient=`
-    +`0xfC2782122A7870811bd5864Ea9C5c67F1d48e863`+`&buyTokenPercentageFee=`
-    +`0.5`
-    
-    // url = `https://ropsten.api.0x.org/swap/v1/quote?sellToken=`+token1+`&buyToken=DAI&buyAmount=3000000000000000000`
+    url = `https://ropsten.api.0x.org/swap/v1/quote?sellToken=` +
+        token1 + `&buyToken=` +
+        token2 + `&sellAmount=` +
+        amountInWei + `&feeRecipient=` +
+        `0xfC2782122A7870811bd5864Ea9C5c67F1d48e863` + `&buyTokenPercentageFee=` +
+        `0.5`
+
+    // url = `https://bsc.api.0x.org/swap/v1/quote?sellToken=BNB&buyToken=1INCH&buyAmount=10000000000000000`
 
     const web3 = new Web3(window.ethereum);
     let accounts = await web3.eth.getAccounts();
@@ -62,52 +61,52 @@ async function swap(token1, token2, amount, tokens){
     console.log(web3.eth.defaultAccount)
 
     const response = await fetch(url).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          response.text().then((text) => {
-              document.getElementById('output').innerHTML = "Error: " + text;
-              throw new Error(text)
-            });
-          throw new Error('Something went wrong');
-        }
-      })
-      .then((responseJson) => {
-        // Do something with the response
-        // console.log(response.json());
-        console.log(responseJson);
-        // const web3 = new Web3(window.ethereum);
-        // let accounts = web3.eth.getAccounts();
-        // web3.eth.defaultAccount = accounts[0]
-        web3.eth.sendTransaction(responseJson, function (error, txhash) {
-            if (error) {
-                // error handling
-                console.log("error")
-                console.log(token2)
-
-                document.getElementById('output').innerHTML = "Error: " + error.message;
-
-                document.getElementById('tokenAddress').innerHTML = tokens.address;
-                document.getElementById('tokenSymbol').innerHTML = tokens.symbol ;
-                document.getElementById('tokenDecimals').innerHTML = tokens.decimals;
-
-                // console.log(tokens.address, tokens.symbol, tokens.decimals)
+            if (response.ok) {
+                return response.json();
+            } else {
+                response.text().then((text) => {
+                    document.getElementById('output').innerHTML = "Error: " + text;
+                    throw new Error(text)
+                });
+                throw new Error('Something went wrong');
             }
-            else {
-                // handling of successful transaction
-                console.log("success")
-                document.getElementById('output').innerHTML = "You received: " + token2;
-                document.getElementById('tokenAddress').innerHTML = tokens.address;
-                document.getElementById('tokenSymbol').innerHTML = tokens.symbol ;
-                document.getElementById('tokenDecimals').innerHTML = tokens.decimals;
+        })
+        .then((responseJson) => {
+            // Do something with the response
+            // console.log(response.json());
+            console.log(responseJson);
+            // const web3 = new Web3(window.ethereum);
+            // let accounts = web3.eth.getAccounts();
+            // web3.eth.defaultAccount = accounts[0]
+            web3.eth.sendTransaction(responseJson, function(error, txhash) {
+                if (error) {
+                    // error handling
+                    console.log("error")
+                    console.log(token2)
 
-                // addToken();
+                    document.getElementById('output').innerHTML = "Error: " + error.message;
 
-            }})
-      })
-      .catch((error) => {
-        console.log(error)
-      });
+                    document.getElementById('tokenAddress').innerHTML = tokens.address;
+                    document.getElementById('tokenSymbol').innerHTML = tokens.symbol;
+                    document.getElementById('tokenDecimals').innerHTML = tokens.decimals;
+
+                    // console.log(tokens.address, tokens.symbol, tokens.decimals)
+                } else {
+                    // handling of successful transaction
+                    console.log("success")
+                    document.getElementById('output').innerHTML = "You received: " + token2;
+                    document.getElementById('tokenAddress').innerHTML = tokens.address;
+                    document.getElementById('tokenSymbol').innerHTML = tokens.symbol;
+                    document.getElementById('tokenDecimals').innerHTML = tokens.decimals;
+
+                    // addToken();
+
+                }
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 
     // if (!response.ok) {
     //     const message = `An error has occured: ${response.text}`;
@@ -116,7 +115,7 @@ async function swap(token1, token2, amount, tokens){
     //     throw new Error(message);
     //   }
 
-    
+
 
     // const web3 = new Web3(window.ethereum);
     // let accounts = await web3.eth.getAccounts();
@@ -124,7 +123,7 @@ async function swap(token1, token2, amount, tokens){
 
     // const response = await fetch(`https://api.0x.org/swap/v0/quote?buyToken=DAI&sellToken=ETH&buyAmount=10000000000000000000`);
 
-    
+
 
     // await web3.eth.sendTransaction(await response.json())
 
@@ -137,9 +136,9 @@ async function swap(token1, token2, amount, tokens){
     //         // handling of successful transaction
     //         console.log("succes")
     //     }})
-        
+
 }
-async function getTokens(){
+async function getTokens() {
 
     const response = await fetch(
         `https://ropsten.api.0x.org/swap/v1/tokens`
@@ -149,7 +148,7 @@ async function getTokens(){
         //`https://api.0x.org/swap/v1/quote?buyToken=DAI&sellToken=ETH&buyAmount=1000000000000000000`
     );
     const tokenResponse = await response.json()
-    //return tokens['symbol']
+        //return tokens['symbol']
     console.log(tokenResponse);
     // console.log(tokenResponse.records[0].symbol)
 
@@ -161,11 +160,11 @@ async function getTokens(){
     }
     console.log(tokens);
 
-    
+
     var token1 = document.getElementById('token1').value;
     var coinIdx2 = Math.floor(Math.random() * tokens.length);
 
-    while (tokens[coinIdx2].symbol == token1){
+    while (tokens[coinIdx2].symbol == token1) {
         coinIdx2 = Math.floor(Math.random() * tokens.length);
     }
 
@@ -175,41 +174,38 @@ async function getTokens(){
 
     const amount = document.getElementById('amountId').value
 
-    swap(token1,tokens[coinIdx2].symbol,amount,tokens[coinIdx2])
+    swap(token1, tokens[coinIdx2].symbol, amount, tokens[coinIdx2])
 
 
 }
 
-async function addToken(){
+async function addToken() {
     const tokenAddress = document.getElementById('tokenAddress').innerHTML;
     const tokenSymbol = document.getElementById('tokenSymbol').innerHTML;
     const tokenDecimals = document.getElementById('tokenDecimals').innerHTML;
     const tokenImage = 'http://placekitten.com/200/300';
 
     try {
-    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-    const wasAdded = await ethereum.request({
-        method: 'wallet_watchAsset',
-        params: {
-        type: 'ERC20', // Initially only supports ERC20, but eventually more!
-        options: {
-            address: tokenAddress, // The address that the token is at.
-            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-            decimals: tokenDecimals, // The number of decimals in the token
-            // image: tokenImage, // A string url of the token logo
-        },
-        },
-    });
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        const wasAdded = await ethereum.request({
+            method: 'wallet_watchAsset',
+            params: {
+                type: 'ERC20', // Initially only supports ERC20, but eventually more!
+                options: {
+                    address: tokenAddress, // The address that the token is at.
+                    symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                    decimals: tokenDecimals, // The number of decimals in the token
+                    // image: tokenImage, // A string url of the token logo
+                },
+            },
+        });
 
-    if (wasAdded) {
-        console.log('Thanks for your interest!');
-    } else {
-        console.log('Your loss!');
-    }
+        if (wasAdded) {
+            console.log('Thanks for your interest!');
+        } else {
+            console.log('Your loss!');
+        }
     } catch (error) {
-    console.log(error);
+        console.log(error);
     }
 }
-
-
-
